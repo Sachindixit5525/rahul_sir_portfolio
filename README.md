ce698f4928e27879fe067494322a30b5dc4b888a
Sachin Dixit <sachindixit5525@gmail.com>
2023-05-01 17:15:13 +0530

    rahul sir portfolio work left

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+<<<<<<< HEAD
 
 # rahul_sir_portfolio
+=======
+# A Portfolio Website - Flutter Web
+
+<img src="/Portfolio.png" alt="portfolio" width="600">
+# test
+# test
+>>>>>>> 07ed30a (first commit)
---
+++ lib/components/default_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({
+    Key? key,
+    this.imageSrc,
+    this.text,
+    this.press,
+  }) : super(key: key);
+
+  final String imageSrc, text;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(50),),),
+      foregroundColor: MaterialStatePropertyAll(Color(0xFFE8F0F9)),
+        padding: MaterialStatePropertyAll(EdgeInsets.symmetric(
+          vertical: kDefaultPadding,
+          horizontal: kDefaultPadding * 2.5,
+        ),),
+      ),
+      onPressed: press,
+      child: Row(
+        children: [
+          Image.asset(imageSrc, height: 40),
+          SizedBox(width: kDefaultPadding),
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
